0ec221885c4dc89249218cd241870630faecabbd
sasaki <sasaki@tagbangers.co.jp>
2017-08-26 17:56:08 +0900

    Add calculate width logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { ResizeService } from './resize.service';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+	styleUrls: ['./app.component.css'],
+	providers: [ResizeService]
 })
 export class AppComponent {
+	constructor(private resizeService: ResizeService) {
+		resizeService.window.subscribe((val) => {
+			if (val) {
+				console.log(val.innerWidth);
+			}
+		});
+	};
 	title = 'app';
 }
