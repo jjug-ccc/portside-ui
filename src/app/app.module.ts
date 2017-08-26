83a5ba3a18f1e4dfef462c28eaf893425ee186f3
ogawa <ogawa@tagbangers.co.jp>
2017-08-26 14:49:35 +0900

    Add page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SessionsComponent } from './pages/sessions/sessions.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ThanksComponent } from './pages/thanks/thanks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SessionsComponent,
+    ProfileComponent,
+    ThanksComponent
   ],
   imports: [
     BrowserModule
